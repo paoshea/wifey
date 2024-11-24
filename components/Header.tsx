bee76db4c33a372649a4f16ed6a9528ef65cdc28
philip.a.oshea <philip.a.oshea@gmail.com>
2024-11-24 09:02:22 -0600

    git tag var 0.1.39

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export function Header() {
+  return (
+    <header className="w-full flex justify-center items-center py-4">
+      <Link href="/" className="flex items-center">
+        <Image
+          src="/logo.svg"
+          alt="Logo"
+          width={120}
+          height={120}
+          priority
+        />
+      </Link>
+    </header>
+  );
+}
