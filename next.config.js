9d94c031b312d9572ff239a96eeac4875906ffc0
philip.a.oshea <philip.a.oshea@gmail.com>
2024-11-24 00:19:42 -0600

    git tag var 0.1.28

--- next.config.js
+++ next.config.js
@@ -1,83 +1,42 @@
 // This file sets up the configuration for Next.js
 const { withSentryConfig } = require('@sentry/nextjs');
 const createNextIntlPlugin = require('next-intl/plugin');
+const { locales, defaultLocale } = require('./lib/i18n/config');
 
 const withNextIntl = createNextIntlPlugin({
-  // Add trailing slash configuration here
-  localePrefix: 'always',
-  locales: ['en', 'es'],
-  defaultLocale: 'en'
+  locales: locales,
+  defaultLocale: defaultLocale,
+  localePrefix: 'always'
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    _next_intl_trailing_slash: '1'
+  }
 };
 
-// Wrap the Sentry config with next-intl
+// Combine next-intl with Sentry config
 module.exports = withNextIntl(withSentryConfig(
   nextConfig,
   {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
-    org: process.env.SENTRY_ORG,
-    project: process.env.SENTRY_PROJECT,
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-    silent: true,
+    org: "ownco-bp",
+    project: "javascript-nextjs",
+    silent: !process.env.CI,
+    widenClientFileUpload: true,
+    reactComponentAnnotation: {
+      enabled: true,
+    },
+    tunnelRoute: "/monitoring",
+    hideSourceMaps: true,
+    disableLogger: true,
+    automaticVercelMonitors: true,
   },
   {
-    // Additional options for Sentry build configuration
     hideSourceMaps: true,
     disableServerWebpackPlugin: process.env.NODE_ENV === 'development',
     disableClientWebpackPlugin: process.env.NODE_ENV === 'development',
   }
 ));
-
-
-// Injected content via Sentry wizard below
-
-const { withSentryConfig } = require("@sentry/nextjs");
-
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    org: "ownco-bp",
-    project: "javascript-nextjs",
-
-    // Only print logs for uploading source maps in CI
-    silent: !process.env.CI,
-
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Automatically annotate React components to show their full name in breadcrumbs and session replay
-    reactComponentAnnotation: {
-      enabled: true,
-    },
-
-    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    tunnelRoute: "/monitoring",
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-  }
-);
