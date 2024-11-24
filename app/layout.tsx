833761151a872f9a317d0a30b2dd7898fc5b593e
philip.a.oshea <philip.a.oshea@gmail.com>
2024-11-24 00:38:10 -0600

    git tag var 0.1.33

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,25 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: [
+    {
+      path: './fonts/GeistVF.woff2',
+      weight: '100 900',
+      style: 'normal',
+    }
+  ],
   variable: "--font-geist-sans",
-  weight: "100 900",
 });
+
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: [
+    {
+      path: './fonts/GeistMonoVF.woff2',
+      weight: '100 900',
+      style: 'normal',
+    }
+  ],
   variable: "--font-geist-mono",
-  weight: "100 900",
 });
 
 export const metadata: Metadata = {
@@ -20,16 +31,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>{children}</body>
     </html>
   );
 }
