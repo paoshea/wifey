c96c5cd2ceb8f7310959fc7de323e9eb2d5f6f1c
philip.a.oshea <philip.a.oshea@gmail.com>
2024-11-24 20:25:06 -0600

    git tag var 0.2.13

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -17,12 +17,12 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         className={cn(
           "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
           {
-            "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-white": variant === "default",
-            "bg-destructive text-destructive-foreground hover:bg-destructive/90 hover:text-white": variant === "destructive",
+            "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground": variant === "default",
+            "bg-destructive text-destructive-foreground hover:bg-destructive/90 hover:text-destructive-foreground": variant === "destructive",
             "border border-input bg-background hover:bg-accent hover:text-accent-foreground": variant === "outline",
-            "bg-secondary text-secondary-foreground hover:bg-secondary/80 hover:text-white": variant === "secondary",
+            "bg-secondary text-secondary-foreground hover:bg-secondary/80 hover:text-secondary-foreground": variant === "secondary",
             "hover:bg-accent hover:text-accent-foreground": variant === "ghost",
-            "text-primary underline-offset-4 hover:underline": variant === "link",
+            "text-primary underline-offset-4 hover:underline hover:text-primary/90": variant === "link",
           },
           {
             "h-10 px-4 py-2": size === "default",
