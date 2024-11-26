21397e18c5a4d2c70f4ffe2360da407bed65f1c2
philip.a.oshea <philip.a.oshea@gmail.com>
2024-11-25 22:36:09 -0600

    git tag var 0.3.24

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,13 +1,12 @@
 'use client';
 
 import * as React from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import { DayPicker, type DayPickerProps } from "react-day-picker";
+import { DayPicker } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
-export type CalendarProps = DayPickerProps;
+export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
 function Calendar({
   className,
@@ -50,10 +49,6 @@ function Calendar({
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
         day_hidden: "invisible",
         ...classNames,
-      }}
-      components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
